3227e8382973a328e86a9312a4f1cefc748e7e6a
malvin <Malvinjose16@gmail.com>
2025-10-29 18:13:00 -0400

    Initial commit

---
+++ app/src/main/java/com/ebanisterialopez/malvin_lopez_ap2_p2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ebanisterialopez.malvin_lopez_ap2_p2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ebanisterialopez.malvin_lopez_ap2_p2.ui.theme.Malvin_lopez_AP2_P2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Malvin_lopez_AP2_P2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Malvin_lopez_AP2_P2Theme {
+        Greeting("Android")
+    }
+}
